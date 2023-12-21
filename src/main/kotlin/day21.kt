c9218b76139bab5d5d7d02490bd506e1540dd848
Ronald van der Lingen <ronald@vdlingen.nl>
2023-12-21 16:48:40 +0100

    day 21 - part 2

--- src/main/kotlin/day21.kt
+++ src/main/kotlin/day21.kt
@@ -22,7 +22,61 @@ fun part1() = with(input) {
 }
 
 fun part2() = with(input) {
-    0L
+    val start = cells.find { it.char == 'S' } ?: error("could not find start")
+
+    fun Coord.safeChar() = Coord(Math.floorMod(x, width), Math.floorMod(y, height)).char
+
+    val visited = mutableSetOf<Coord>()
+
+    var frontier = mutableSetOf<Coord>()
+    frontier.add(start)
+
+    var count = 0L
+    var countOther = 0L
+
+    val frontiers = Array(width) { 0 }
+    val delta1 = Array(width) { 0 }
+    val delta2 = Array(width) { 0 }
+
+    var step = 0
+    while (true) {
+        val newFrontier = mutableSetOf<Coord>()
+
+        frontier.forEach { p ->
+            p.unvalidatedNeighbors(Directions.Orthogonal).forEach { n ->
+                if (n.safeChar() != '#') {
+                    if (visited.add(n)) newFrontier.add(n)
+                }
+            }
+        }
+
+        count = (countOther + newFrontier.size).also { countOther = count }
+
+        val index = step % width
+        if (step >= width) {
+            val delta = newFrontier.size - frontiers[index]
+
+            delta2[index] = delta - delta1[index]
+            delta1[index] = delta
+        }
+        frontiers[index] = newFrontier.size
+
+        frontier = newFrontier
+        step++
+
+        // iterate until the deltas are stable
+        if (step >= 2 * width && delta2.all { it == 0 }) break
+    }
+
+    // interpolate the remaining steps
+    (step..<26501365).forEach {
+        (it % width).let { index ->
+            frontiers[index] += delta1[index]
+            count = (countOther + frontiers[index]).also { countOther = count }
+        }
+    }
+
+    count
 }
 
 fun main() {
